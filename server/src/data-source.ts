fd2385b193706e594fd7055a5fddedbf28c26a4f
yunanah <bbyl6319@gmail.com>
2022-10-20 14:32:07 +0900

    [feature-001] 개발환경 세팅

---
+++ server/src/data-source.ts
@@ -0,0 +1,16 @@
+import "reflect-metadata";
+import { DataSource } from "typeorm";
+
+export const AppDataSource = new DataSource({
+  type: "postgres",
+  host: "localhost",
+  port: 5432,
+  username: "postgres",
+  password: "password",
+  database: "postgres",
+  synchronize: true,
+  logging: false,
+  entities: ["src/entities/**/*.ts"],
+  migrations: [],
+  subscribers: [],
+});
